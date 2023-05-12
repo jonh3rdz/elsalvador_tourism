a938e66737f723e0cac50411d204977e8f7c3c90
Jonh rdz <j.rodriguez2018ng@clases.edu.sv>
2023-05-12 09:49:17 -0600

    hotels StoreHotelRequest

--- app/Http/Requests/API/V1/Hotel/StoreHotelRequest.php
+++ app/Http/Requests/API/V1/Hotel/StoreHotelRequest.php
@@ -11,7 +11,7 @@ class StoreHotelRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -19,10 +19,48 @@ class StoreHotelRequest extends FormRequest
      *
      * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array|string>
      */
-    public function rules(): array
+    public function rules()
     {
         return [
-            //
+            'name' => 'required|max:255',
+            'description' => 'required|max:255',
+            'location' => 'required|max:255',
+            'image_url' => 'required|max:255',
+            'price' => 'required|numeric',
+            'opening_hours' => 'required|max:255',
+            'contact_address' => 'required|max:255',
+            'contact_phone' => 'required|max:255',
+            'contact_email' => 'required|email|max:255',
+            'destination_id' => 'required|exists:destinations,id'
+        ];
+    }
+
+    public function messages()
+    {
+        return [
+            'required' => 'El campo :attribute es obligatorio.',
+            'max' => 'El campo :attribute no debe tener más de :max caracteres.',
+            'min' => 'El valor mínimo permitido para :attribute es :min.',
+            'numeric' => 'El campo :attribute debe ser numérico.',
+            'exists' => 'El valor seleccionado para :attribute no es válido.',
+            'email' => 'El campo :attribute debe ser una dirección de correo electrónico válida.',
+            'unique' => 'El valor ingresado para :attribute ya existe en la base de datos.',
+
+            'accepted' => 'Debes aceptar los términos y condiciones.',
+            'alpha_num' => 'El campo :attribute debe contener solo letras y números.',
+            'after' => 'El campo :attribute debe ser una fecha posterior a :date.',
+            'before' => 'El campo :attribute debe ser una fecha anterior a :date.',
+            'date' => 'El campo :attribute debe ser una fecha válida.',
+            'boolean' => 'El campo debe ser verdadero o falso.',
+            'digits' => 'El campo :attribute debe tener :value dígitos.',
+            'digits_between' => 'El campo :attribute debe tener entre :min y :max dígitos.',
+            'file' => 'El campo :attribute debe ser un archivo.',
+            'filled' => 'El campo debe tener un valor (no vacío).',
+            'image' => 'El archivo seleccionado para :attribute debe ser una imagen.',
+            'in' => 'El valor seleccionado para :attribute no es válido.',
+            'integer' => 'El campo :attribute debe ser un número entero.',
+            'nullable' => 'El campo :attribute puede ser nulo.',
+            'required_if' => 'El campo :attribute es obligatorio cuando el campo :other es :value.',
         ];
     }
 }
